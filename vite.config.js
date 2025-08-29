397113dad2c1d64a6ef7baeae4875d5c548ff626
nuriskandarsyah <nuriskandarsyah@gmail.com>
2025-08-30 04:01:33 +0700

    Initial commit UMKM Karangsong project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +7,10 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    server: {
+        watch: {
+            usePolling: true, // membantu live reload di beberapa hosting
+        },
+    },
 });
